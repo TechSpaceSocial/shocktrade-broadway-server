b6d48a5686c25bb9f9a64de10cf58fc31c56f76f
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-01-05 09:44:32 -0800

    throttling messages at a rate of 250 messages/second and logging the throughput

--- src/main/scala/com/shocktrade/narratives/EodDataImportNarrative.scala
+++ src/main/scala/com/shocktrade/narratives/EodDataImportNarrative.scala
@@ -10,13 +10,14 @@ import com.ldaniels528.broadway.core.actors.Actors._
 import com.ldaniels528.broadway.core.actors.FileReadingActor.{CopyText, Delimited, _}
 import com.ldaniels528.broadway.core.actors.kafka.avro.KafkaAvroPublishingActor
 import com.ldaniels528.broadway.core.actors.kafka.avro.KafkaAvroPublishingActor._
-import com.ldaniels528.broadway.core.actors.{FileReadingActor, ThroughputCalculatingActor}
+import com.ldaniels528.broadway.core.actors.{FileReadingActor, ThrottlingActor, ThroughputCalculatingActor}
 import com.ldaniels528.broadway.core.resources._
 import com.ldaniels528.broadway.server.ServerConfig
 import com.ldaniels528.trifecta.util.StringHelper._
 import com.shocktrade.helpers.ConversionHelper._
 import com.shocktrade.helpers.ResourceTracker
 import com.shocktrade.narratives.EodDataImportNarrative._
+import org.slf4j.LoggerFactory
 
 /**
  * EODData.com Import Narrative
@@ -24,19 +25,29 @@ import com.shocktrade.narratives.EodDataImportNarrative._
  */
 class EodDataImportNarrative(config: ServerConfig) extends BroadwayNarrative(config, "EOD Data Import")
 with KafkaConstants {
+  private lazy val logger = LoggerFactory.getLogger(getClass)
+
   // create a file reader actor to read lines from the incoming resource
   val fileReader = addActor(new FileReadingActor(config))
 
   // create a Kafka publishing actor
   val kafkaPublisher = addActor(new KafkaAvroPublishingActor(eodDataTopic, brokers))
 
-  // let's throttle the messages flowing into Kafka
-  //val proxy = addActor(new ThrottlingActor(kafkaPublisher, rateLimit = 100))
+  // let's calculate the throughput of the Kafka publishing actor
+  var ticker = 0
+  val throughputCalc = addActor(new ThroughputCalculatingActor(kafkaPublisher, { messagesPerSecond =>
+    // log the throughput every 5 seconds
+    if (ticker % 5 == 0) {
+      logger.info(f"KafkaPublisher: Throughput rate is $messagesPerSecond%.1f")
+    }
+    ticker += 1
+  }))
 
-  val proxy = addActor(new ThroughputCalculatingActor(label = "KafkaAvroPublishing", kafkaPublisher))
+  // let's throttle the messages flowing into Kafka
+  val throttler = addActor(new ThrottlingActor(throughputCalc, rateLimit = 250))
 
   // create a EOD data transformation actor
-  val eodDataToAvroActor = addActor(new EodDataToAvroActor(proxy))
+  val eodDataToAvroActor = addActor(new EodDataToAvroActor(throttler))
 
   onStart { resource =>
     // start the processing by submitting a request to the file reader actor
