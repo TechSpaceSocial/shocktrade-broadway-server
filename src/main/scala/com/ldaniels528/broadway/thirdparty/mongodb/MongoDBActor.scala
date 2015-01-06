51d12145d97a6f55eeef2f68f754242fee2f8cd1
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-01-05 19:11:01 -0800

    added find and findOne commands

--- src/main/scala/com/ldaniels528/broadway/thirdparty/mongodb/MongoDBActor.scala
+++ src/main/scala/com/ldaniels528/broadway/thirdparty/mongodb/MongoDBActor.scala
@@ -1,6 +1,8 @@
 package com.ldaniels528.broadway.thirdparty.mongodb
 
 import akka.actor.Actor
+import com.ldaniels528.broadway.core.actors.Actors.BWxActorRef
+import com.ldaniels528.broadway.core.actors.Actors.Implicits._
 import com.ldaniels528.broadway.thirdparty.mongodb.MongoDBActor._
 import com.mongodb.ServerAddress
 import com.mongodb.casbah.Imports._
@@ -8,14 +10,16 @@ import com.mongodb.casbah.Imports._
 import scala.collection.concurrent.TrieMap
 
 /**
- * MongoDB Actor
+ * The MongoDB Actor is capable of executing `find`, `findOne`, `insert`, `update`, and `upsert` commands
+ * against a MongoDB server instance. NOTE: `find` and `findOne` queries require an actor a a recipient for retrieved
+ * records.
  * @author Lawrence Daniels <lawrence.daniels@gmail.com>
  */
 class MongoDBActor(databaseName: String, serverList: String) extends Actor {
   private val collections = TrieMap[String, MongoCollection]()
   private var conn_? : Option[MongoConnection] = None
 
-  override def preStart() = conn_? = Option(getConnection( parseServerList(serverList)))
+  override def preStart() = conn_? = Option(getConnection(parseServerList(serverList)))
 
   override def postStop() {
     conn_?.foreach(_.close())
@@ -23,6 +27,18 @@ class MongoDBActor(databaseName: String, serverList: String) extends Actor {
   }
 
   override def receive = {
+    case Find(collection, query, fields, recipient) =>
+      val mc = getCollection(collection)
+      mc.foreach(_.find(query, fields) foreach { o =>
+        recipient ! MongoResult(o)
+      })
+
+    case FindOne(collection, query, fields, recipient) =>
+      val mc = getCollection(collection)
+      mc.foreach(_.findOne(query, fields) foreach { o =>
+        recipient ! MongoResult(o)
+      })
+
     case Insert(collection, doc, concern) =>
       val mc = getCollection(collection)
       mc.foreach(_.insert(doc))
@@ -64,6 +80,11 @@ object MongoDBActor {
     MongoConnection(addresses, options)
   }
 
+  /**
+   * Parses a MongoDB server list
+   * @param serverList the given MongoDB server list (e.g. "dev601:27017,dev602:27017,dev603:27017")
+   * @return a [[List]] of [[ServerAddress]] objects
+   */
   def parseServerList(serverList: String): List[ServerAddress] = {
     serverList.split("[,]").toList map { server =>
       server.split("[:]").toList match {
@@ -73,8 +94,14 @@ object MongoDBActor {
       }
     }
   }
-  
+
+  case class FindOne(collection: String, query: DBObject, fields: DBObject, recipient: BWxActorRef)
+
+  case class Find(collection: String, query: DBObject, fields: DBObject, recipient: BWxActorRef)
+
   case class Insert(collection: String, doc: DBObject, concern: WriteConcern = WriteConcern.JournalSafe)
+
+  case class MongoResult(o: DBObject)
 
   case class Update(collection: String,
                     query: DBObject,
