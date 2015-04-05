456968c6b48c594dbfc144ac376d5baa02d4ff79
Lawrence Daniels <lawrence.daniels@gmail.com>
2015-04-05 12:19:47 -0700

    moved streaming actor out of the HTTP server class

--- src/main/scala/com/ldaniels528/broadway/server/http/BroadwayHttpServer.scala
+++ src/main/scala/com/ldaniels528/broadway/server/http/BroadwayHttpServer.scala
@@ -1,13 +1,12 @@
 package com.ldaniels528.broadway.server.http
 
-import akka.actor.{Actor, ActorLogging, ActorRef, Props, _}
+import akka.actor.{Props, _}
 import akka.io.IO
 import akka.io.Tcp.Bound
 import akka.pattern.ask
 import akka.util.Timeout
 import org.slf4j.LoggerFactory
 import spray.can.Http
-import spray.http._
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
@@ -33,45 +32,5 @@ class BroadwayHttpServer(host: String, port: Int)(implicit system: ActorSystem) 
         logger.error(s"Unexpected response $outcome")
     }
   }
-
-}
-
-/**
- * Broadway Http Server Singleton
- * @author Lawrence Daniels <lawrence.daniels@gmail.com>
- */
-object BroadwayHttpServer {
-  private[this] lazy val logger = LoggerFactory.getLogger(getClass)
-
-  class StreamingActor(client: ActorRef, count: Int) extends Actor with ActorLogging {
-
-    import context.dispatcher
-
-    log.debug("Starting streaming response ...")
-
-    // we use the successful sending of a chunk as trigger for scheduling the next chunk
-    client ! ChunkedResponseStart(HttpResponse(entity = " " * 2048)).withAck(Ok(count))
-
-    def receive = {
-      case Ok(0) =>
-        log.info("Finalizing response stream ...")
-        client ! MessageChunk("\nStopped...")
-        client ! ChunkedMessageEnd
-        context.stop(self)
-
-      case Ok(remaining) =>
-        log.info("Sending response chunk ...")
-        context.system.scheduler.scheduleOnce(100 millis span) {
-          client ! MessageChunk(DateTime.now.toIsoDateTimeString + ", ").withAck(Ok(remaining - 1))
-        }
-
-      case x: Http.ConnectionClosed =>
-        log.info("Canceling response stream due to {} ...", x)
-        context.stop(self)
-    }
-  }
-
-  // simple case class whose instances we use as send confirmation message for streaming chunks
-  case class Ok(remaining: Int)
 
 }
---
+++ src/main/scala/com/ldaniels528/broadway/server/http/StreamingActor.scala
@@ -0,0 +1,8 @@
+package com.ldaniels528.broadway.server.http
+
+/**
+ * Created by ldaniels on 4/5/15.
+ */
+class StreamingActor {
+
+}
